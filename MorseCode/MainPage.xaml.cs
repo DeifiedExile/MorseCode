74e56ec991bfdef6c43dc96156e7eea331f0b7f3
Lucas <lwolfs@my.wctc.edu>
2020-02-23 20:06:19 -0600

    Add project files.

---
+++ MorseCode/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCode
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Dot_Click(object sender, EventArgs e)
+        {
+            Letter.Text += ".";
+        }
+        private void Dash_Click(object sender, EventArgs e)
+        {
+            Letter.Text += "-";
+        }
+
+        private void Space_Click(object sender, EventArgs e)
+        {
+            Morse morse = new Morse();
+            Message.Text += Morse.MorseCoder(Letter.Text.ToString());
+            Letter.Text = "";
+        }
+    }
+}
